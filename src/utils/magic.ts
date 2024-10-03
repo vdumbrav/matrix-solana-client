06dd213b67f111864775b193a08c260096350ff4
Vlad Dumbava <vladislav.dumbrava@gmail.com>
2024-10-03 19:12:10 +0300

    feat(utils): add OAuth and Auth extensions to Magic SDK

--- src/utils/magic.ts
+++ src/utils/magic.ts
@@ -1,9 +1,13 @@
 import { Magic } from 'magic-sdk';
+import { OAuthExtension } from '@magic-ext/oauth';
+import { AuthExtension } from '@magic-ext/auth';
 import { SolanaExtension } from '@magic-ext/solana';
 import { clusterApiUrl } from '@solana/web3.js';
 
 const magic = new Magic(import.meta.env.VITE_MAGIC_PUBLISHABLE_KEY, {
   extensions: [
+    new OAuthExtension(),
+    new AuthExtension(),
     new SolanaExtension({
       rpcUrl: clusterApiUrl('devnet'),
     }),
