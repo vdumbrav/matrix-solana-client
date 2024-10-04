ae87f015e2487691756dacabc6e4ed92cc9ab048
Vlad Dumbava <vladislav.dumbrava@gmail.com>
2024-10-04 20:26:45 +0300

    refactor(wallets): remove PhantomWalletAdapter from WalletSetup

--- src/components/WalletSetup.tsx
+++ src/components/WalletSetup.tsx
@@ -1,7 +1,7 @@
 import { useMemo } from 'react';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
 import { WalletModalProvider, WalletMultiButton, WalletDisconnectButton } from '@solana/wallet-adapter-react-ui';
 import { clusterApiUrl } from '@solana/web3.js';
 import { Wallet } from './Wallet/Wallet';
@@ -18,7 +18,7 @@ export const WalletSetup = ({ matrixClient, roomId }: IProps) => {
   const network = WalletAdapterNetwork.Devnet;
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
-  const wallets = useMemo(() => [new PhantomWalletAdapter(), new SolflareWalletAdapter({ network })], [network]);
+  const wallets = useMemo(() => [new SolflareWalletAdapter({ network })], [network]);
 
   return (
     <ConnectionProvider endpoint={endpoint}>
